5e0130ee4a9071d3d9083ca549265f39dc333f50
GOHAR ALI <161229437+Goharali-Learning@users.noreply.github.com>
2024-03-26 04:32:34 -0700

    Add files via upload

---
+++ Task 09(favourite Number)/main.ts
@@ -0,0 +1,17 @@
+//TASK 09 (FAVOURITE_NUMBER)
+
+//QUESTIOn:09
+
+//Favorite Number: Store your favorite number in a variable. Then, using 
+//that variable, create a message that reveals your favorite number. Print that message.
+
+
+
+
+
+
+
+let favouriteNumber : number = 420;
+console.log(`My favourite number is ${favouriteNumber}`);
+
+
