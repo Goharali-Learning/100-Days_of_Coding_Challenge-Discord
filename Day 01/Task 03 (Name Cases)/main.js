541949d986c3373bee75d010ae6405fb53bd4148
GOHAR ALI <161229437+Goharali-Learning@users.noreply.github.com>
2024-03-26 04:30:24 -0700

    Add files via upload

---
+++ Day 01/Task 03 (Name Cases)/main.js
@@ -0,0 +1,11 @@
+//TASk 03 (Name Cases)
+//QUESTION:03
+//Name Cases: Store a person’s name in a variable, and then print that person’s 
+//name in lowercase, uppercase, and titlecase.
+//Lower_Case
+var personName = "Gohar";
+console.log("lowercase:", personName.toLowerCase());
+//Upper_Case
+console.log("uppercase:", personName.toUpperCase());
+//Title_Case
+console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toLowerCase(); }));
