177c6f29d8ffa239e4b544e4f94df0847e29ee2d
GOHAR ALI <161229437+Goharali-Learning@users.noreply.github.com>
2024-03-27 03:40:57 -0700

    Delete Day 06/Task 19 (Dinner Guests) directory

--- Day 06/Task 19 (Dinner Guests)/main.js
+++
@@ -1,8 +0,0 @@
-//Task 14 (Guests List)
-var guest_list = ["Nadeem", "Murtaza", "Mehmood", "Tanveer", "Sameer"];
-// for (let i=0; i<guest_list.length; i++){
-// console.log(`Dear ` + guest_list[i] + `\n\nYou are invited for a dinner tommorow.\n\nThank You.\n`);
-// }
-//Task 19 (Dinner Guests)
-var guest_length = guest_list.length;
-console.log("We are inviting total ".concat(guest_length, " guests."));
